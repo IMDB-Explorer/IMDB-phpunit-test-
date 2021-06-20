ede8e68e5f30143ced7f9c302de1cc4634ca7e9d
roksana <r.mirzayi.1999@gmail.com>
2021-06-20 23:17:33 +0430

    a few test added

--- tests/unit/MovieModelTest.php
+++ tests/unit/MovieModelTest.php
@@ -10,6 +10,35 @@ class MovieModelTest extends \PHPUnit\Framework\TestCase
         $this->user = new \App\application\model\MovieModel;
     }
 
+    // /** @test */
+    // public function getMovieGenre()
+    // {
+    //     $genre= array(0=>array('movie_id' => '1',
+    //                                 'genre'=>'اجتماعی',
+    //                                 'created_at' => '2020-07-24 11:21:00',
+    //                                 'updated_at'=> null,
+    //                                ));
+    //     $this -> user-> getMovieGenre(1);
+    //     $this -> assertEquals($this -> user-> getMovieGenre(1), $genre);
+    // }
+    // /** @test */
+    // public function getMovieStars()
+    // {
+    //     $actors= array(0=>Array('first_name' => 'پیمان',
+    //                                 'laset_name'=>'مَعادی',
+    //                                 'picture' => 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/12/Payman_Maadi_2019.jpg/220px-Payman_Maadi_2019.jpg',
+    //                                 'id'=>'10'
+    //                                ));
+    //     $this->user-> getMovieStars(2);
+    //     $this->assertEquals($this->user->getMovieStars(2), $actors);
+    // }
+    /** @test */
+    public function topMovies()
+    {
+        $watchlist= array(0=>Array('COUNT(`movie_id`)' => '0'));
+        $this->user ->watchlistNumberOfRows(0);
+        $this->assertEquals($this->user ->watchlistNumberOfRows(0),$watchlist);
+    }
     /** @test */
     public function storeComments()
     {
